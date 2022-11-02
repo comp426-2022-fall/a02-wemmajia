f72aabc6b06f88663386212267498026b026e688
Emma Jia <emmajia@unc.edu>
2022-11-02 10:05:11 -0400

    cli

--- cli.js
+++ cli.js
@@ -18,6 +18,53 @@ if(args.h) {
     process.exit(0);
 }
 
+let timezone = moment.tz.guess();
+if(args.z) {
+    timezone = args.z;
+}
 
+let latitude = 0;
+let longitude = 0;
+if(args.n) {
+    latitude = args.n;
+} else if (args.s) {
+    latitude = -args.s;
+} else {
+    console.log('Latitude must be in range');
+}
+if(args.e) {
+    longitude = args.e;
+} else if (args.w) {
+    longitude = -args.w;
+} else {
+    console.log('Longitude must be in range');
+}
 
+let day = 1;
+if(args.d) {
+    day = args.d;
+}
 
+const response = await fetch('https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&daily=precipitation_hours&timezone=${timezone}');
+const data = await response.json();
+
+if(args.j) {
+    console.log(data);
+}
+
+const days = args.d 
+let result = ''
+if(data.daily.precipitation_hours[days] != 0){
+    result = 'You might need your galoshes ';
+} else {
+    result = "You probably won't need your galoshes ";
+}
+if (days == 0) {
+  result += "today.";
+} else if (days > 1) {
+  result += "in " + days + " days.";
+} else {
+  result += "tomorrow.";
+}
+
+console.log(result);
